ae15ccc3dbf6f68b004609678fb293f7f1d20a74
begininva <begininva@pbx.dominanta-bb.ru>
2013-09-17 12:37:58 +0800

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Asterisk::Application.config.secret_key_base = 'd02444bd5d79ebe7082b04fddf7363288b75a176a66c31bced01919e76420c523261b79448449f65c5a16750a56cb7b7ae8dca9d4081cb042d575923a7969147'
